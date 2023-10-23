37b3c192844991f3c3961dff25eedec3e1fafe77
sdlkhfksl <97817853+sdlkhfksl@users.noreply.github.com>
2023-10-23 15:36:10 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -41,7 +41,7 @@
 		// showLog: false
 	});
 	let url = new URL(location.href).searchParams.get("url") || "https://wordpress.zealken.repl.co/videos.m3u8";
-	let playUrl = new URL(location.href).searchParams.get("playUrl") || "https://wordpress.zealken.repl.co/0.m3u";
+	let playUrl = new URL(location.href).searchParams.get("playUrl") || "https://wordpress.zealken.repl.co/all.m3u";
 	url = url.trim();
 	playUrl = playUrl.trim();
 	if (playUrl.length > 0) {
